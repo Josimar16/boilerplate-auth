a1bdf2a6b22537bb3a3e90980f4407dc562fb755
Josimar16 <josimarjr479@gmail.com>
2021-10-26 21:23:13 -0300

    refactor: :recycle: Refatorando arquivos iniciais do backend
    configurando o base path e o cors

---
+++ backend/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT || 3333);
+}
+bootstrap();
